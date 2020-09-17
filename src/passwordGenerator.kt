fe22d892239fdb72802e68643e6f3bde70658ff5
Eric <54077752+Ericgacoki@users.noreply.github.com>
2020-09-18 00:33:30 +0300

    Update passwordGenerator.kt

--- src/passwordGenerator.kt
+++ src/passwordGenerator.kt
@@ -5,7 +5,7 @@ fun main() {
 // define
 
 private fun generateNums(start: Int = 10, end: Int = 99) =
-            (start..end).shuffled()[4].toString() + // 4 is 
+            (start..end).shuffled()[4].toString() + // 4 is g
             (start..end).shuffled().last().toString()
 
 private fun generateSpecialChar(): String {
